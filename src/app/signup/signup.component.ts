5b814f8609a316ee4847f096e0bb884410e9a4cc
SK AQUIB <aquib.javed@navsoft.in>
2020-12-23 19:56:35 +0530

    second-commit

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -14,9 +14,28 @@ export class SignupComponent implements OnInit {
   public submitted: boolean = false;
   public agreeTandc: boolean = false;
 
+  public allClassData = [
+    {
+    'courseName':'Secondary'
+    },
+   {
+    'courseName':'Higher-Secondary'
+   },
+   {
+    'courseName':'Graduation'
+    }
+]
+
   constructor(private formBuilder: FormBuilder, private toastr: ToastrService, private router: Router) { }
 
   ngOnInit(): void {
+
+    let arr=[];
+    for(let i=0;i< this.allClassData.length;i++)
+    {
+      arr.push(this.BuildFormDynamic(this.allClassData[i]))
+
+    }
 
     this.signupForm = this.formBuilder.group({
       name: ['', [Validators.required]],
@@ -25,12 +44,28 @@ export class SignupComponent implements OnInit {
       password: ['', [Validators.required, Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[#?!@$%^&*-]).{8,}$')]],
       dob: ['', [Validators.required]],
       terms: ['', [Validators.required]],
+      qualification:this.formBuilder.array(arr)
     })
-
   }
+
+  BuildFormDynamic(classDatas):FormGroup{
+    return this.formBuilder.group({
+          class:[classDatas.courseName],
+          passingYear:[''],
+          collegeName:[''],
+          percentage :['']
+     })
+   }
 
 
   public onSubmit = () => {
+
+    let arr=[];
+    for(let i=0;i< this.allClassData.length;i++)
+    {
+      arr.push(this.BuildFormDynamic(this.allClassData[i]))
+
+    }
 
     /*     this.toastr.success('Signup up Successfully!', 'Success!'); */
       }
